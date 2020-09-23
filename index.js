8bf1e25e74399039e1842ac11c2b6bc906c79b22
Andrew Yahner <andrew.yahner@acadiasoft.com>
2020-09-23 17:16:46 -0400

    changed greeting again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there #2' });
+  res.send({ hi: 'there #3' });
 });
 
 const PORT = process.env.PORT || 5000;
